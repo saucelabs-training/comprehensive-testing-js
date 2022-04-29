2d7b6ab47ca7262cdf789511d019ffe86add1a33
nadvolod <nadvolod@gmail.com>
2022-04-29 10:15:28 -0400

    updating init and visual:ci script

--- test/specs/visual.solution.spec.js
+++ test/specs/visual.solution.spec.js
@@ -1,7 +1,7 @@
 describe('My React app', () => {
   it('should look correct', async () => {
     await browser.url('');
-    await browser.execute('/*@visual.init*/', 'My React App');
+    await browser.execute('/*@visual.init*/', 'My Solution');
     await browser.execute('/*@visual.snapshot*/', 'Home Page');
 
     const result = await browser.execute('/*@visual.end*/');
